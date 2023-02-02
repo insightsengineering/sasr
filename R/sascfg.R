3a94f1091d05cad08559e15dadbc0520dfcbfc19
gravesti <isaac.gravestock@roche.com>
2023-02-02 16:41:21 +0100

    change sas to saspath

--- R/sascfg.R
+++ R/sascfg.R
@@ -5,7 +5,7 @@
 #'
 #' @param name (`character`)\cr name of the configuration.
 #' @param host (`character`)\cr host name of remote server.
-#' @param sas (`character`)\cr SAS executable path on remote server.
+#' @param saspath (`character`)\cr SAS executable path on remote server.
 #' @param ssh (`character`)\cr executable path of ssh.
 #' @param encoding (`character`)\cr encoding of the SAS session.
 #' @param ... additional arguments.
@@ -14,13 +14,13 @@
 #'
 #' @export
 #' @details
-#' `host` and `sas` are required to connect to remote SAS server. Other arguments can follow default.
+#' `host` and `saspath` are required to connect to remote SAS server. Other arguments can follow default.
 #' If transferring datasets is needed, then tunnelling is required.
 #' Use `tunnel = `, `rtunnel = ` to specify tunnels and reverse tunnels.
 #' The values should be length 1 integer.
-sascfg <- function(name = "default", host, sas, ssh = system("which ssh", intern = TRUE),
+sascfg <- function(name = "default", host, saspath, ssh = system("which ssh", intern = TRUE),
                    encoding = "latin1", options = list("-fullstimer"), ..., sascfg = "sascfg_personal.py") {
-  lst <- list(host = host, sas = sas, ssh = ssh, encoding = encoding, options = options)
+  lst <- list(host = host, saspath = saspath, ssh = ssh, encoding = encoding, options = options)
   lst <- c(lst, list(...))
   check_list(lst, types = c("character", "integer"), names = "named")
   f <- file(sascfg, "w")
--- tests/testthat/test-sascfg.R
+++ tests/testthat/test-sascfg.R
@@ -3,7 +3,7 @@
 test_that("sascfg creates sas configuration file", {
   skip_if_not(py_available(TRUE))
   tmpf <- tempfile()
-  sascfg(ssh = "ssh", sas = "sas", host = "test.com", sascfg = tmpf)
+  sascfg(ssh = "ssh", saspath = "sas", host = "test.com", sascfg = tmpf)
   on.exit(unlink(tmpf))
   expect_file_exists(tmpf)
 })
